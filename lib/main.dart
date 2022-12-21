3ca980dd05110974ed80de61d3fd01a12f43d685
CervinoB <jcervinobarbosa@gmail.com>
2022-12-21 18:54:21 -0300

    creating the transaction adpater

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_expenses_app/model/transaction.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+
+  Hive.registerAdapter(TransactionAdapter());
 
   runApp(MyApp());
 }
